e58a504efc31dac3bdd615bfb436274a5497366e
milan <milan@localhost>
2013-05-05 22:12:33 +0200

    works now

--- setTime.pl
+++ setTime.pl
@@ -1,26 +1,61 @@
 #!/usr/bin/perl
 
-use Time::HiRes;
-use Net::Telnet ();
-
-# sudo apt-get install libnet-telnet-perl libtime-hires-perl
+use Time::HiRes qw(time sleep);
+use Net::Telnet;
+use DateTime;
+use Data::Dumper;
+# sudo apt-get install libnet-telnet-perl libtime-hires-perl libdatetime-perl
 
 my $host='192.168.179.101';
 my $port='1000';
+my $time_zone='Europe/Berlin';
+my $sleep =60;
 
 $t = new Net::Telnet (
 	Timeout => 10,
-	Prompt => '/bash\$ $/'
+	ErrMode => \&reconnect
 );
 
-$t->open(
-	Host => $host,
-	Port => $port
-);
+reconnect();
+my $dt=DateTime->now();
 
 while(1){
-	my $time=int(Time::HiRes::gettimeofday()*1000);
-	print $time."\n";
-	my @result=$t->cmd($time);
-	print @result."\n";
+	my $epoch=time();
+	$dt = DateTime->from_epoch( epoch => $epoch, time_zone => $time_zone);
+	print "INFO: "
+	.$dt->ymd()
+	." ".$dt->hms().".".substr($dt->nanosecond(),0,3)
+	."\tepoch:".$epoch
+	#." ".$dt->epoch()
+	."\n";
+
+	#add current timezone
+	$epoch+=$dt->offset();	
+	
+	#lower and higher 32bit
+	#print "INFO: ".(int($epoch*1000)/0xFFFFFFFF)." ".(int($epoch*1000)%0xFFFFFFFF)."\n";
+
+	#build command
+	my $cmd="t".int($epoch*1000);
+	print "< $cmd\n";
+
+	#output result
+	$result=join("\n",$t->cmd($cmd));
+	$result=~s/\n+$//;
+	print "> $result\n";
+
+	sleep $sleep;
 }
+
+sub reconnect{
+	print "INFO: close...\n";
+	$t->close();
+	sleep(1);
+
+	print "INFO: connect...\n";
+	$t->open(
+		Host => $host,
+		Port => $port
+	);
+}
+
